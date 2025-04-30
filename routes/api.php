193b5cf92718051136bae1f9d15011453a90f623
Naufal F <naufalfirman098@gmail.com>
2025-04-30 16:21:42 +0700

    feat(api): login & register api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Resources\User as ResourcesUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::get('/users', function (Request $request) {
+//     return new ResourcesUser(User::paginate());
+// });
+
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::resource('users', UserController::class);
+
+        Route::get('/profile', [AuthController::class, 'profile']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+});
