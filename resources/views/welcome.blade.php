425206ac2c6944393e7c3eb12af5ef35d790135d
Naufal F <naufalfirman098@gmail.com>
2025-05-02 23:31:57 +0700

    change link api doc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     <div class="flex flex-col justify-center items-center h-screen gap-2">
         <h1>REST API Sistem Gudang</h1>
         <div class="flex gap-2">
-            <span>API Documentation: </span><a href="http://127.0.0.1:8000/api/documentation"
+            <span>API Documentation: </span><a href="https://postman.co/workspace/My-Workspace~e63a8d8b-c82e-4684-8f67-4dd9f0c22362/collection/11047813-daa7a9c1-6f04-43af-969f-b973fe5c12db?action=share&creator=11047813"
                 class="text-blue-500 hover:underline">click here</a>
         </div>
     </div>
